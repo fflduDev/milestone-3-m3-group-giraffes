73ea3c78582b6fb1e70f5fc82276f52e1287a18c
JackCallanan <john.callanan1@student.fairfield.edu>
2025-04-02 15:13:05 -0400

    added directreport and depthfirst

--- tree/OrgChartImpl.java
+++ tree/OrgChartImpl.java
@@ -1,7 +1,6 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class OrgChartImpl implements OrgChart{
 
@@ -10,8 +9,10 @@ public class OrgChartImpl implements OrgChart{
 	private GenericTreeNode<Employee> root;
 
 	public void addRoot(Employee e) {
-		root = new GenericTreeNode<>(e);
-		nodes.add(root);
+		if(root == null) {
+			GenericTreeNode<Employee> newRoot = new GenericTreeNode<>(e);
+			nodes.add(newRoot);
+		}
 	}
 	// if there is no orgchart, start it
 
@@ -24,6 +25,17 @@ public class OrgChartImpl implements OrgChart{
 
 	public void addDirectReport(Employee manager, Employee newPerson) {
 
+		for(int i =0; i < nodes.size(); i++) {
+			GenericTreeNode<Employee> currentEmployee = nodes.get(i);
+			if(currentEmployee.data.equals(manager)){
+				GenericTreeNode<Employee> newEmployee = new GenericTreeNode<>(newPerson);
+
+				currentEmployee.addChild(newEmployee); //adds child to current employee (manager)
+				nodes.add(newEmployee);//add the new employee to the tree
+				break;
+			}
+
+		}
 	}
 	// add the newPerson as a direct report (child) of manager
 
@@ -33,10 +45,32 @@ public class OrgChartImpl implements OrgChart{
 	// remove the employee, give their direct reports to their supervisor
 
 	public void showOrgChartDepthFirst() {
+		System.out.println("Org Chart (DFS) of the company is:");
+		Queue<GenericTreeNode<Employee>> queue = new LinkedList<>();
+		ArrayList<Employee> alreadyVisited= new ArrayList<>();
+
+		for(int i = 0; i <nodes.size(); i++) {
+			queue.add(nodes.get(i));
+
+			while(!queue.isEmpty()) {
+				GenericTreeNode<Employee> currentEmployee = queue.remove();
+
+			if(!alreadyVisited.contains(currentEmployee.data)) {
+				alreadyVisited.add(currentEmployee.data);
+				}
+			if(currentEmployee.children.size() > 0) {
+				queue.addAll(currentEmployee.children);
+				}
+			}
+
+		}
+		for(Employee e: alreadyVisited){
+			System.out.println(e);
+		}
 
 	}
 
 	public void showOrgChartBreadthFirst() {
 	}
-	
+
 }
